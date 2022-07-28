2a703a4fc962f93460abbaa2a81c30e990d2a93a
Admin <hemamane5376@gmail.com>
2022-07-28 16:14:57 +0530

    assignment-1

---
+++ dom.js
@@ -0,0 +1,13 @@
+  document.querySelector(".class1").innerText="Hema Mane"
+document.querySelector(".class2").innerText="I am Hema mane from Karnataka i am recently grauated from gug univesrsity in Electronics field,learning web development "
+document.querySelector(".class3").style.color="red"
+document.querySelectorAll(".divs")[0].style.border ="black solid"
+document.querySelectorAll(".divs")[0].innerText="Fourth"
+document.querySelectorAll(".divs")[1].style.border="black solid"
+document.querySelectorAll(".divs")[1].innerText="Fifth"
+document.querySelectorAll(".divs")[2].style.border="black solid"
+document.querySelectorAll(".divs")[2].innerText="Sixth"
+document.querySelector("#id1").appendChild("a").innerText
+document.querySelector("div")[7]
+
+
